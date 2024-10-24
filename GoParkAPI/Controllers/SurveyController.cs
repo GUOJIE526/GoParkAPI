9bba875d1fc20caaf6f4f23e15f612cd63d1637f
menghsiu <menghsiu0120@gmail.com>
2024-10-24 13:48:32 +0800

    新增了意見回覆API

---
+++ GoParkAPI/Controllers/SurveyController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using GoParkAPI.DTO;
+using GoParkAPI.Models;
+
+namespace GoParkAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SurveyController : ControllerBase
+    {
+        private readonly EasyParkContext _context;
+
+        public SurveyController(EasyParkContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostSurvey(SurveyDTO survey)
+        {
+            if (ModelState.IsValid)
+            {
+                Survey newsurvey = new Survey {
+                    Id = 0,
+                    UserId = survey.UserId,
+                    Question = survey.Question,
+                    IsReplied = false,
+                    SubmittedAt = DateTime.Now,
+                    Status = "未回覆"
+                };
+
+                _context.Survey.Add(newsurvey);
+                await _context.SaveChangesAsync();
+
+                return new JsonResult(new { status = "success" });
+            }
+            else
+            {
+                return new JsonResult(new { status = "fail" });
+            }
+
+            
+        }
+
+    }
+}
