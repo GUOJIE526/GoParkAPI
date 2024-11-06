fb6202636baa8c55c6d19927aa9c380629aa6621
yunjane <yunyun89116@gmail.com>
2024-11-06 10:52:04 +0800

    1106忘記密碼&重設密碼

--- GoParkAPI/DTO/ChangePswDTO.cs
+++ GoParkAPI/DTO/ChangePswDTO.cs
@@ -5,4 +5,15 @@
         public string OldPassword { get; set; }
         public string NewPassword { get; set; }
     }
+    public class ForgotDTO
+    {
+        public string Email { get; set; }
+    }
+
+    public class ResetDTO
+    {
+        public string Token { get; set; }
+        public string NewPassword { get; set; }
+    }
+
 }
